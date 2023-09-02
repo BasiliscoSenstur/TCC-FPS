3c2f746e51c6dfc3c60454932a913ac4b3a5d0c6
Senstur Games <senstur.art@gmail.com>
2023-09-01 23:13:34 -0300

    Alfa Commit

---
+++ TCC-FPS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Moviment")]
+    public CharacterController controller;
+    public float moveSpeed;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
