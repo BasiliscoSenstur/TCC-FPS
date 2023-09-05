c6e54cf9ff412eb0ab08923ea99d978c53063328
Senstur Games <senstur.art@gmail.com>
2023-09-04 22:54:48 -0300

    1.8

---
+++ TCC-FPS/Assets/_Project/Scripts/Enemy/MovingTargetController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MovingTargetController : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+        
+    }
+}
